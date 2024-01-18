73f09e657efd642213dd738628ee31b18d3f6305
Nick <nnmankowski@owu.edu>
2024-01-17 23:06:34 -0500

    Forgot some files

---
+++ src/bsp_tree.cpp
@@ -0,0 +1,72 @@
+#include <math.h>
+
+#include "common_struct.h"
+#include "bsp_tree.h"
+#include "geom.h"
+#include "map.h"
+
+bool is_front(const Vertex& vertex, const Wall* wall) {
+
+    // Look at z component of cross product of wall and vertex
+    Vect3<float> line_vect = {
+        .a = wall->line.v2.x - wall->line.v1.x,
+        .b = wall->line.v2.y - wall->line.v1.y,
+        .c = wall->line.v2.z - wall->line.v1.z,
+    };    
+    
+    Vect3<float> v1_to_vert_vect = {
+        .a = vertex.x - wall->line.v1.x,
+        .b = vertex.y - wall->line.v1.y,
+        .c = vertex.z - wall->line.v1.z,
+    };
+
+    Vect3<float> cross_product = CrossProduct(line_vect, v1_to_vert_vect);
+
+    return cross_product.c > 0;
+}
+
+bool is_front(Wall& test_wall, Wall* divider_ptr) {
+    Vertex midpoint = Midpoint(test_wall);
+    return is_front(midpoint, divider_ptr);
+}
+
+BSP_Tree* _DeserializeBSPHelper(const std::string& bsp_string, int& pos) {
+
+    // Base case, we have reached a leaf
+    if (bsp_string[pos] == '#') {
+        pos++;
+        return NULL;
+    }
+
+    // Read id
+    pos += 4; // Skip past (id=
+    std::string id_str = "";
+    while (bsp_string[pos] != ',')
+        id_str += bsp_string[pos++];
+    int id = std::stoi(id_str);
+
+    // Read the front node
+    pos += 7; // Skip past ,front=
+    BSP_Tree* front_subtree = _DeserializeBSPHelper(bsp_string, pos);
+
+    // Read the back node
+    pos += 6; // Skip past ,back=
+    BSP_Tree* back_subtree = _DeserializeBSPHelper(bsp_string, pos);
+
+    // Skip past the closing )
+    pos++;
+
+    // Assemble the root node
+    BSP_Tree* root_tree = new BSP_Tree;
+    root_tree->id = id;
+    root_tree->front = front_subtree;
+    root_tree->back = back_subtree;
+
+    // Return it
+    return root_tree;
+}
+
+BSP_Tree DeseralizeBSP(const std::string& bsp_string) {
+    int pos = 0;
+    return *(_DeserializeBSPHelper(bsp_string, pos));
+}
---
+++ src/minimap.cpp
@@ -0,0 +1,49 @@
+#include "minimap.h"
+
+MinimapHandler::MinimapHandler(Camera& newCamera, Map& newMap, StdGraphicsHandler& newStdGraphicsHandler) {
+    camera = &newCamera;
+    map = &newMap;
+    stdGraphicsHandler = &newStdGraphicsHandler;
+}
+
+MinimapHandler::~MinimapHandler() { }
+
+int MinimapHandler::MapToScreenX(double x) {
+    return (int)(x * MINIMAP_SCALE) + MINIMAP_X + (MINIMAP_WIDTH / 2);
+}
+
+int MinimapHandler::MapToScreenY(double y) {
+    return (int)(y * MINIMAP_SCALE) + MINIMAP_Y + (MINIMAP_HEIGHT / 2);
+}
+
+void MinimapHandler::DrawPlayer() {
+    int center_x = MapToScreenX(camera->x);
+    int center_y = MapToScreenY(camera->y);
+
+    int dx = MINIMAP_ANGLE_INDICATOR_SIZE * cos(camera->angle + (M_PI / 2)); // TODO: abstract away "magic value"
+    int dy = MINIMAP_ANGLE_INDICATOR_SIZE * sin(camera->angle + (M_PI / 2));
+
+    stdGraphicsHandler->DrawLine(center_x, center_y, center_x + dx, center_y + dy, BLACK);
+    stdGraphicsHandler->DrawCircle(center_x, center_y, MINIMAP_CAMERA_RADIUS, WHITE);
+}
+
+// Draw the minimap
+void MinimapHandler::Draw() {
+    for (int i = 0; i < map->walls.size(); i++) {
+        int x1 = MapToScreenX(map->walls[i].line.v1.x);
+        int y1 = MapToScreenY(map->walls[i].line.v1.y);
+
+        int x2 = MapToScreenX(map->walls[i].line.v2.x);
+        int y2 = MapToScreenY(map->walls[i].line.v2.y);
+
+        stdGraphicsHandler->DrawLine(x1, y1, x2, y2, map->walls[i].color[0], map->walls[i].color[1], map->walls[i].color[2]);
+    }
+
+    for (int i = 0; i < map->vertices.size(); i++) {
+        int x = MapToScreenX(map->vertices[i].x);
+        int y = MapToScreenY(map->vertices[i].y);
+        stdGraphicsHandler->DrawCircle(x, y, MINIMAP_VERTEX_RADIUS, BLACK);
+    }
+
+    DrawPlayer();
+}
